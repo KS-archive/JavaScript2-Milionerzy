4179691da01ce843282b59c87b05f0d9709932e1
Konrad Szwarc <konrad.szwarc@outlook.com>
2018-07-18 01:49:56 +0200

    Add Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'main.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
